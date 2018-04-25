56eaafd7a7e9635113fa1bc39260bea350727666
qmac <quinten.mcnamara@gmail.com>
2018-04-25 18:03:37 -0500

    mvpa update

--- mvpa.py
+++ mvpa.py
@@ -8,6 +8,7 @@ from nilearn._utils.niimg_conversions import check_niimg
 from nistats.design_matrix import make_design_matrix
 
 from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
 from sklearn.svm import LinearSVC
@@ -28,7 +29,7 @@ def get_labels(event_file, n_scans):
 
 
 def get_data_matrix(data):
-    fir_delays = [0]#[1, 2, 4, 5]
+    fir_delays = [3, 4, 5]
     rolls = []
     for f in fir_delays:
         roll = np.roll(data.T, -f, axis=0)
@@ -51,6 +52,37 @@ def partition_data(X, Y):
     return X_train, X_test, Y_train, Y_test
 
 
+def run_single_subject(image_files, event_file, mask_file):
+    # Load imaging data
+    mask = check_niimg(mask_file, ensure_ndim=3).get_data().astype(bool)
+    print('Mask shape: ' + str(mask.shape))
+    print('Number of masked voxels: ' + str(np.sum(mask)))
+
+    labels = np.around(get_labels(event_file, 975).as_matrix()).T[0]
+
+    img = check_niimg(image_files[0], ensure_ndim=4)
+    img_data = img.get_data()[mask]
+    rolled = get_data_matrix(img_data)
+    X_train, X_test, Y_train, Y_test = partition_data(rolled, labels)
+    print('Training matrix: ' + str(X_train.shape))
+    print('Testing matrix: ' + str(X_test.shape))
+    print('Training labels shape: ' + str(Y_train.shape))
+    print('Testing labels shape: ' + str(Y_test.shape))
+
+    # Reduce dimensionality
+    dim_red = SelectKBest(f_classif, k=1000)
+    X_train = dim_red.fit_transform(X_train, Y_train)
+    X_test = dim_red.transform(X_test)
+    print('Dimensionality reduced, new data shape: ' + str(X_train.shape))
+
+    # Run classification
+    clf = LogisticRegression()
+    clf.fit(X_train, Y_train)
+    y_preds = clf.predict(X_test)
+    test_acc_score = accuracy_score(Y_test, y_preds)
+    print('Test accuracy score: ' + str(test_acc_score))
+    print('Random test: ' + str(Y_test.mean()))
+
 def run_analysis(image_files, event_file, mask_file):
     # Load imaging data
     mask = check_niimg(mask_file, ensure_ndim=3).get_data().astype(bool)
@@ -58,7 +90,7 @@ def run_analysis(image_files, event_file, mask_file):
     print('Number of masked voxels: ' + str(np.sum(mask)))
 
     X_train = []
-    for image_file in image_files[:11]:
+    for image_file in image_files[1:11]:
         img = check_niimg(image_file, ensure_ndim=4)
         img_data = img.get_data()[mask]
         rolled = get_data_matrix(img_data)
@@ -73,8 +105,6 @@ def run_analysis(image_files, event_file, mask_file):
 
     # Get labels
     labels = np.around(get_labels(event_file, 975).as_matrix()).T[0]
-    labels = np.roll(labels, 4)
-    labels[:4] = 0
     Y_train = np.concatenate([labels] * 10)#(len(image_files) - 1))
     print('Labels shape: ' + str(Y_train.shape))
 
@@ -86,22 +116,22 @@ def run_analysis(image_files, event_file, mask_file):
     print('Test accuracy score: ' + str(test_acc_score))
     print('Random test: ' + str(labels.mean()))
 
-    # # Reduce dimensionality
-    # dim_red = SelectKBest(f_classif, k=1000)
-    # X_train = dim_red.fit_transform(X_train, Y_train)
-    # X_test = dim_red.transform(X_test)
-    # print('Dimensionality reduced, new data shape: ' + str(X_train.shape))
+    # Reduce dimensionality
+    dim_red = SelectKBest(f_classif, k=1000)
+    X_train = dim_red.fit_transform(X_train, Y_train)
+    X_test = dim_red.transform(X_test)
+    print('Dimensionality reduced, new data shape: ' + str(X_train.shape))
 
-    # # Re-run classification
-    # clf = LinearSVC(class_weight='balanced')
-    # clf.fit(X_train, Y_train)
-    # y_preds = clf.predict(X_test)
-    # test_acc_score = accuracy_score(labels, y_preds)
-    # print('Dimension reduced test accuracy score: ' + str(test_acc_score))
+    # Re-run classification
+    clf = LinearSVC(class_weight='balanced')
+    clf.fit(X_train, Y_train)
+    y_preds = clf.predict(X_test)
+    test_acc_score = accuracy_score(labels, y_preds)
+    print('Dimension reduced test accuracy score: ' + str(test_acc_score))
 
 
 if __name__ == '__main__':
     event_file = sys.argv[1]
     mask_file = sys.argv[2]
     image_files = sys.argv[3:]
-    run_analysis(image_files, event_file, mask_file)
+    run_single_subject(image_files, event_file, mask_file)
