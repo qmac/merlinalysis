6c26a917a9ebd8355230839314ad62d4bd8290cf
qmac <quinten.mcnamara@gmail.com>
2018-05-02 02:43:32 -0500

    downsample for single events

--- utils/downsample_events.py
+++ utils/downsample_events.py
@@ -12,14 +12,27 @@ def downsample_events(df, chunk_size=10):
         values.extend([totals.argmax()] * min(chunk_size, 1002 - c))
     return values
 
+def downsample_single_events(df, chunk_size=10):
+    values = []
+    for c in range(27, 1002, chunk_size):
+        start = c * 1.5
+        end = (c + chunk_size - 1) * 1.5
+        subframe = df.loc[start:end]
+        totals = subframe.sum()[0]
+        val = 1.0 if totals >= 5 else 0.0
+        values.extend([val] * min(chunk_size, 1002 - c))
+    return values
+
 if __name__ == '__main__':
     event_file = sys.argv[1]
     output_file = sys.argv[2]
     df = pd.read_csv(event_file, index_col=0)
     df = df[df['onset'] >= 40.5]
     df = df.sort_values('onset').reset_index(drop=True)
+    df = df.drop_duplicates()
     pivoted = df.pivot(index='onset', columns='trial_type', values='modulation')
     pivoted = pivoted.fillna(0.0)
+    # new_df = pd.DataFrame(downsample_single_events(pivoted), columns=['trial_type'])
     new_df = pd.DataFrame(downsample_events(pivoted), columns=['trial_type'])
     onsets = np.array(range(27, 1002)) * 1.5
     new_df['onset'] = onsets
