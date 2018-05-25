7326fba8620282c3d574b16b877ecccb60eac808
qmac <quinten.mcnamara@gmail.com>
2018-05-25 15:22:41 -0500

    extract features cleanup

--- extract_features.py
+++ extract_features.py
@@ -1,98 +1,64 @@
 ''' Extracts features from a video stimulus'''
 
-import sys
 import numpy as np
 import pandas as pd
-from pliers.converters import (IBMSpeechAPIConverter,
-                               VideoToAudioConverter)
+from pliers.converters import VideoToAudioConverter
 from pliers.extractors import (GoogleVisionAPILabelExtractor,
                                GoogleVisionAPIFaceExtractor,
                                WordEmbeddingExtractor,
                                RMSEExtractor,
-                               BrightnessExtractor,
-                               STFTAudioExtractor,
                                merge_results)
 from pliers.filters import FrameSamplingFilter
-from pliers.stimuli import VideoStim, TextStim
+from pliers.stimuli import VideoStim, TextStim, ComplexTextStim
 
 
-WORD2VEC_PATH = '/Users/quinnmac/Documents/00-Documents-Archive/College Senior Year/'\
-                'Semester 2/NLP/Final Project/GoogleNews-vectors-negative300.bin'
-GLOVE_PATH = '/Users/quinnmac/Documents/codenames/glove.6B/glove.6B.300d.txt.word2vec'
+WORD2VEC_PATH = '/Users/quinnmac/Documents/00-Documents-Archive/'\
+                'College Senior Year/Semester 2/NLP/Final Project/'\
+                'GoogleNews-vectors-negative300.bin'
+GLOVE_PATH = '/Users/quinnmac/Documents/00-Documents-Archive/'\
+             'codenames/glove.6B/glove.6B.300d.txt.word2vec'
 TR = 1.5
 
 
-def to_long_format(df):
-    ''' Convert from wide to long format, making each row a single
-    feature/value pair.
-    Args:
-        df (DataFrame): a timeline that is currently in wide format
-    '''
-    if isinstance(df.columns, pd.core.index.MultiIndex):
-        ids = list(filter(lambda x: x[1] is '', df.columns))
-        variables = ['extractor', 'feature']
+def extract_speech(stims):
+    onsets, durations = zip(*[(e.onset, e.duration) for e in stims.elements])
+    res = pd.DataFrame({'onset': onsets,
+                        'duration': durations,
+                        'modulation': [1.0] * len(onsets),
+                        'trial_type': 'speech'})
+    res.to_csv('events/audio_speech_events.csv')
+
+
+def extract_audio_semantics(stims, glove=True):
+    if glove:
+        ext = WordEmbeddingExtractor(GLOVE_PATH, binary=False)
+        out = 'events/audio_glove_events.csv'
     else:
-        df = df.reset_index() if not isinstance(
-            df.index, pd.Int64Index) else df
-        ids = list(set(df.columns) & set(['stim', 'onset', 'duration']))
-        variables = 'feature'
-
-    values = list(set(df.columns) - set(ids))
-    converted = pd.melt(df, id_vars=ids, value_vars=values, var_name=variables)
-    converted.columns = [
-        c[0] if isinstance(c, tuple) else c for c in converted.columns]
-    return converted
-
-
-def parse_p2fa(transcript_path):
-    with open(transcript_path) as f:
-        start_parse = False
-        all_lines = f.readlines()
-        texts = []
-        for i, line in enumerate(all_lines):
-            if line == '\titem [2]:\n':
-                start_parse = True
-            if start_parse and line.startswith('\t\t\ti'):
-                movie_offset = 40.5
-                onset = float(all_lines[i+1].split()[-1])
-                duration = float(all_lines[i+2].split()[-1]) - onset
-                text = str(all_lines[i+3].split()[-1])[1:-1].lower()
-                if not (text == 'sp'):
-                    texts.append(TextStim(text=text, onset=onset+movie_offset, duration=duration))
-
-    return texts
-
-
-def extract_audio_semantics(stims):
-    if isinstance(stims, VideoStim):
-        speech_text_converter = IBMSpeechAPIConverter()
-        stims = speech_text_converter.transform(stims)
-        stims.save('stims/transcription/ibm_transcript.txt')
-    ext = WordEmbeddingExtractor(WORD2VEC_PATH, binary=True)
+        ext = WordEmbeddingExtractor(WORD2VEC_PATH, binary=True)
+        out = 'events/audio_semantic_events.csv'
     results = ext.transform(stims)
-    res = merge_results(results, metadata=False, flatten_columns=True)
+    res = merge_results(results, metadata=False, flatten_columns=True,
+                        format='long')
     res = res.drop(['object_id', 'order'], axis=1)
-    res = to_long_format(res)
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
-    res.to_csv('events/audio_semantic_events.csv')
+    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'},
+               inplace=True)
+    res.to_csv(out)
 
 
-def extract_audio_gloves(stims):
-    if isinstance(stims, VideoStim):
-        speech_text_converter = IBMSpeechAPIConverter()
-        stims = speech_text_converter.transform(stims)
-        stims.save('stims/transcription/ibm_transcript.txt')
-    ext = WordEmbeddingExtractor(GLOVE_PATH, binary=False)
-    results = ext.transform(stims)
-    res = merge_results(results, metadata=False, flatten_columns=True)
+def extract_audio_energy(video):
+    aud = VideoToAudioConverter().transform(video)
+    frame_length = int(aud.sampling_rate*TR)
+    ext = RMSEExtractor(frame_length=frame_length, hop_length=frame_length,
+                        center=False)
+    res = ext.transform(aud).to_df(metadata=False, format='long')
+    res['onset'] += TR
     res = res.drop(['object_id', 'order'], axis=1)
-    res = to_long_format(res)
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
-    res.to_csv('events/audio_glove_events.csv')
+    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'},
+               inplace=True)
+    res.to_csv('events/audio_energy_events.csv')
 
 
 def extract_image_labels(video, save_frames=False):
-    # Sample frames at TR
     frame_sampling_filter = FrameSamplingFilter(hertz=1)
     sampled_video = frame_sampling_filter.transform(video)
 
@@ -109,7 +75,8 @@ def extract_image_labels(video, save_frames=False):
 
     # Clean and write out data
     res = res.fillna(0)
-    res['GoogleVisionAPILabelExtractor'] = np.round(res['GoogleVisionAPILabelExtractor'])
+    label_key = 'GoogleVisionAPILabelExtractor'
+    res[label_key] = np.round(res[label_key])
     new_cols = []
     for col in res.columns.values:
         if col[0].startswith('Google'):
@@ -123,142 +90,84 @@ def extract_image_labels(video, save_frames=False):
 def extract_visual_objects(visual_events):
     res = pd.DataFrame.from_csv(visual_events)
     res = res.drop(['order', 'object_id'], axis=1)
-    res = to_long_format(res)
+
+    # Convert to long format
+    ids = ['onset', 'duration']
+    values = list(set(res.columns) - set(ids))
+    res = pd.melt(res, id_vars=ids, value_vars=values, var_name='trial_type')
+
     # Slicing here to get rid of b''
-    res['feature'] = [v[2:-1] for v in res['feature']]
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
+    res['trial_type'] = [v[2:-1] for v in res['trial_type']]
+    res.rename(columns={'value': 'modulation'}, inplace=True)
     res.to_csv('events/visual_object_events.csv')
 
 
-def extract_visual_semantics(visual_events):
+def extract_visual_semantics(visual_events, glove=True):
     res = pd.DataFrame.from_csv(visual_events)
     onsets = res['onset']
     durations = res['duration']
     res = res.drop(['onset', 'duration', 'order', 'object_id'], axis=1)
-    words = res.apply(lambda x: list(res.columns[x.values.astype('bool')]), axis=1)
+    words = res.apply(lambda x: list(res.columns[x.values.astype('bool')]),
+                      axis=1)
 
     texts = []
     for tags, o, d in zip(words, onsets, durations):
         for w in tags:
             # Slicing here to get rid of b''
             texts.append(TextStim(text=w[2:-1], onset=o, duration=d))
-    ext = WordEmbeddingExtractor(WORD2VEC_PATH, binary=True)
+
+    if glove:
+        ext = WordEmbeddingExtractor(GLOVE_PATH, binary=False)
+        out = 'events/visual_semantic_events.csv'
+    else:
+        ext = WordEmbeddingExtractor(WORD2VEC_PATH, binary=True)
+        out = 'events/visual_glove_events.csv'
     results = ext.transform(texts)
-    res = merge_results(results, metadata=False, flatten_columns=True)
-    res = res.drop('duration', axis=1)
+    res = merge_results(results, metadata=False, flatten_columns=True,
+                        format='long')
+    res = res.drop(['object_id', 'order', 'duration'], axis=1)
     res = res.groupby('onset').sum().reset_index()
     res['duration'] = durations
-    res = res.drop(['object_id', 'order'], axis=1)
-    res = to_long_format(res)
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
-    res.to_csv('events/visual_semantic_events.csv')
-
-
-def extract_glove_semantics(visual_events):
-    res = pd.DataFrame.from_csv(visual_events)
-    onsets = res['onset']
-    durations = res['duration']
-    res = res.drop(['onset', 'duration', 'order', 'object_id'], axis=1)
-    words = res.apply(lambda x: list(res.columns[x.values.astype('bool')]), axis=1)
-
-    texts = []
-    for tags, o, d in zip(words, onsets, durations):
-        for w in tags:
-            # Slicing here to get rid of b''
-            texts.append(TextStim(text=w[2:-1], onset=o, duration=d))
-    ext = WordEmbeddingExtractor(GLOVE_PATH, binary=False)
-    results = ext.transform(texts)
-    res = merge_results(results, metadata=False, flatten_columns=True)
-    res = res.drop('duration', axis=1)
-    res = res.groupby('onset').sum().reset_index()
-    res['duration'] = durations
-    res = res.drop(['object_id', 'order'], axis=1)
-    res = to_long_format(res)
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
-    res.to_csv('events/visual_glove_events.csv')
-
-
-def extract_audio_energy(video):
-    aud = VideoToAudioConverter().transform(video)
-    frame_length = int(aud.sampling_rate*TR)
-    ext = RMSEExtractor(frame_length=frame_length, hop_length=frame_length, center=False)
-    res = ext.transform(aud).to_df(metadata=False, format='long')
-    res['onset'] += TR
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
-    res.to_csv('events/audio_energy_events.csv')
-
-
-def extract_brightness(video):
-    frame_sampling_filter = FrameSamplingFilter(every=5)
-    sampled_video = frame_sampling_filter.transform(video)
-    ext = BrightnessExtractor()
-    res = ext.transform(sampled_video)
-    res = merge_results(res, metadata=False, flatten_columns=True, format='long')
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
-    res.to_csv('events/visual_brightness_events.csv')
+    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'},
+               inplace=True)
+    res.to_csv(out)
 
 
 def extract_faces(video):
-    # Sample frames at TR
     frame_sampling_filter = FrameSamplingFilter(hertz=1)
     sampled_video = frame_sampling_filter.transform(video)
 
-    # Use a Vision API to extract object labels
     ext = GoogleVisionAPIFaceExtractor()
     results = ext.transform(sampled_video)
-    res = merge_results(results, metadata=False, format='long', extractor_names=False, object_id=False)
+    res = merge_results(results, metadata=False, format='long',
+                        extractor_names=False, object_id=False)
 
-    # Clean and write out data
     res = res[res['feature'] == 'face_detectionConfidence']
     res = res.drop(['order'], axis=1)
     res = res.fillna(0)
     res['value'] = np.round(res['value'])
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
+    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'},
+               inplace=True)
     res.to_csv('events/visual_face_events.csv')
 
 
-def extract_speech():
-    speech_events = pd.read_csv('events/audio_semantic_events.csv')
-    vals = [1.0] * len(speech_events['onset'])
-    res = pd.DataFrame({'onset': speech_events['onset'],
-                        'duration': speech_events['duration'],
-                        'speech': vals})
-    res = to_long_format(res)
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
-    res.to_csv('events/speech_events.csv')
-
-
-def extract_spectrogram(video):
-    aud = VideoToAudioConverter().transform(video)
-    frame_length = TR
-    ext = STFTAudioExtractor(frame_size=frame_length, hop_size=frame_length, freq_bins=80)
-    spectrogram = ext.transform(aud)
-    res = spectrogram.to_df(metadata=False, format='long')
-    res['onset'] += TR
-    res.rename(columns={'value': 'modulation', 'feature': 'trial_type'}, inplace=True)
-    res.to_csv('events/audio_spectrogram_events.csv')
-
-
 if __name__ == '__main__':
-    if len(sys.argv) != 1:
-        print('Usage: python extract_features.py')
-        sys.exit(0)
-
     video = VideoStim('stims/Merlin.mp4')
-    # extract_image_labels(video, save_frames=False)
-    # print('Done with label extraction')
-    # extract_visual_semantics('events/raw_visual_events.csv')
-    # print('Done with visual semantics extraction')
-    # extract_glove_semantics('events/raw_visual_events.csv')
-    # print('Done with visual glove semantics extraction')
-    # extract_audio_gloves(parse_p2fa('stims/transcription/Merlin_trimmed.TextGrid'))
-    # print('Done with audio glove semantics extraction')
-    # extract_visual_objects('events/raw_visual_events.csv')
-    # print('Done with visual object extraction')
-    # extract_audio_semantics(parse_p2fa('stims/transcription/Merlin_trimmed.TextGrid'))
-    # print('Done with audio semantics extraction')
-    # extract_audio_energy(video)
-    # extract_speech()
-    # extract_brightness(video)
-    # extract_faces(video)
-    # extract_spectrogram(video)
+    transcript = ComplexTextStim('stims/transcription/aligned_transcript.txt')
+    for e in transcript.elements:
+        e.onset += 40.5
+
+    extract_speech(transcript)
+    print('Done with speech extraction')
+    extract_audio_semantics(transcript, glove=True)
+    print('Done with audio semantics extraction')
+    extract_audio_energy(video)
+    print('Done with audio energy extraction')
+    extract_image_labels(video, save_frames=False)
+    print('Done with label extraction')
+    extract_visual_semantics('events/raw_visual_events.csv', glove=True)
+    print('Done with visual semantics extraction')
+    extract_visual_objects('events/raw_visual_events.csv')
+    print('Done with visual object extraction')
+    extract_faces(video)
+    print('Done with face extraction')
